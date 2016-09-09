5875191c15405178e252307f1148024c9d72d52e
Mārtiņš Bērtiņš <martins.bertins@wunderkraut.com>
2016-09-09 10:55:07 +0200

    Remove the instagram service; Move the functions to instagram formatter plugin

--- src/Plugin/Field/FieldFormatter/AmpInstagramFormatter.php
+++ src/Plugin/Field/FieldFormatter/AmpInstagramFormatter.php
@@ -2,8 +2,6 @@
 
 namespace Drupal\amp\Plugin\Field\FieldFormatter;
 
-use Drupal\amp\AmpInstagramShortcode;
-use Drupal\Component\Utility\Html;
 use Drupal\Core\Field\FieldDefinitionInterface;
 use Drupal\Core\Field\FieldItemInterface;
 use Drupal\Core\Field\FieldItemListInterface;
@@ -23,44 +21,15 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  *   }
  * )
  */
-class AmpInstagramFormatter extends FormatterBase implements ContainerFactoryPluginInterface {
+class AmpInstagramFormatter extends FormatterBase {
 
   /**
-   * @var \Drupal\amp\AmpInstagramShortcode $ampInstagramShortcode
+   * @var array
    */
-  protected $ampInstagramShortcode;
-
-  /**
-   * AmpInstagramFormatter constructor.
-   * @param string $plugin_id
-   * @param mixed $plugin_definition
-   * @param \Drupal\Core\Field\FieldDefinitionInterface $field_definition
-   * @param array $settings
-   * @param string $label
-   * @param string $view_mode
-   * @param array $third_party_settings
-   * @param \Drupal\amp\AmpInstagramShortcode $ampInstagramShortcode
-   */
-  public function __construct($plugin_id, $plugin_definition, FieldDefinitionInterface $field_definition, array $settings, $label, $view_mode, array $third_party_settings, AmpInstagramShortcode $ampInstagramShortcode) {
-    parent::__construct($plugin_id, $plugin_definition, $field_definition, $settings, $label, $view_mode, $third_party_settings);
-    $this->ampInstagramShortcode = $ampInstagramShortcode;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-    return new static(
-      $plugin_id,
-      $plugin_definition,
-      $configuration['field_definition'],
-      $configuration['settings'],
-      $configuration['label'],
-      $configuration['view_mode'],
-      $configuration['third_party_settings'],
-      $container->get('amp.instagram_shortcode')
-    );
-  }
+  private static $patterns = [
+    '@((http|https):){0,1}//(www\.){0,1}instagram\.com/p/(?<shortcode>[a-z0-9_-]+)@i' => 'shortcode',
+    '@((http|https):){0,1}//(www\.){0,1}instagr\.am/p/(?<shortcode>[a-z0-9_-]+)@i' => 'shortcode',
+  ];
 
   /**
    * {@inheritdoc}
@@ -137,8 +106,8 @@ class AmpInstagramFormatter extends FormatterBase implements ContainerFactoryPlu
     $settings = $this->getSettings();
     foreach ($items as $delta => $item) {
       $matches = [];
-      foreach (AmpInstagramShortcode::$validationRegexp as $pattern => $key) {
-        if (preg_match($pattern, $this->ampInstagramShortcode->getSourceValue($item), $matches)) {
+      foreach (self::$patterns as $pattern => $key) {
+        if (preg_match($pattern, $this->getInstagramCode($item), $matches)) {
           break;
         }
       }
@@ -170,6 +139,29 @@ class AmpInstagramFormatter extends FormatterBase implements ContainerFactoryPlu
       'nodisplay' => 'nodisplay',
       'responsive' => 'responsive',
     ];
+  }
+
+  /**
+   * Extracts the raw code from input.
+   * Copied over from media_entity module.
+   *
+   * @param mixed $value
+   *   The input value. Can be a normal string or a value wrapped by the
+   *   Typed Data API.
+   *
+   * @return string|null
+   */
+  private function getInstagramCode($value) {
+    if (is_string($value)) {
+      return $value;
+    }
+    elseif ($value instanceof FieldItemInterface) {
+      $class = get_class($value);
+      $property = $class::mainPropertyName();
+      if ($property) {
+        return $value->$property;
+      }
+    }
   }
 
 }
